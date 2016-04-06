b7289ded7c6276b9a346c47e0cdcd3daf4f81e83
Jane <jane.e.reynolds@gmail.com>
2016-04-05 22:21:34 -0700

    Style event item buttons

--- pollinatorweek/js/events.js
+++ pollinatorweek/js/events.js
@@ -25,18 +25,18 @@ $(document).ready(function(){
     	for (let event of data.rows) {
     		// check to see if new state section necessary
     		if (event.state !== last_state) {
-    			$('#events_list').append("<h2>"+event.state+"</h2>");
+    			$('#events_list').append("<h3>"+event.state+"</h3>");
     		}
     		
     		// Create Event Title button
     		$('#events_list').append(
     			"<button class='event-item btn'>"
-    			+ "<h3>"+event.event_name+"</h3>"
+    			+ "<h2>"+event.event_name+"</h2>"
     			+ "<span class='fa fa-chevron-circle-down'></span>"
     			+ "<div class='row'>"
-    			+ "<div class='col-sm-6'><h4>"+event.city+"</h4></div>"
-    			+ "<div class='col-sm-6'>"
-    			+ "<h4>"+event.date+"</h4>"
+    			+ "<div class='col-xs-6'><h4><span class='highlight'>"+event.city+"</span></h4></div>"
+    			+ "<div class='col-xs-6'>"
+    			+ "<h4>"+event.date.replace(/\/2016/,"")+",</h4>"
     			+ "<h4>"+event.time+"</h4></div>"
     			+ "</div>"
     			+ "</button>"
