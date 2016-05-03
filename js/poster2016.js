729c809354805adc37ff71e82014e730e7f3a23a
Jane <jane.e.reynolds@gmail.com>
2016-05-02 17:46:56 -0700

    Establish basic method for displaying tree/pollinator name on marker hover over

--- js/poster2016.js
+++ js/poster2016.js
@@ -10,8 +10,27 @@ $(document).ready(function(){
 	$("#markers-container").height(poster_height);
 	
 	// Position markers
-	$("#1").css({left: '12%', top: '29%'});
-	$("#2").css({right: '12%', top: '25%'});
+	$("#1").css({left: '11%', top: '28%'});
+	$("#2").css({right: '8%', top: '25%'});
+	
+	
+	// Add pop-ups
+	var content_width = $("#info-window").width();
+	$("#1").hover(
+		function(){
+			var marker_position = $(this).position();
+			//alert(marker_position.left + ", " + marker_position.top);
+			var left = marker_position.left + 50;
+			var top = marker_position.top;
+			$("#info-window").css({left: left, top: top}).html("Bigleaf Maple"); 
+			content_width = $("#info-window").width(); 
+			$("#info-window").width(content_width + 20);  // Add extra width for padding
+			$("#info-window").toggle(); },
+		function(){
+			$("#info-window").toggle();
+			$("#info-window").width(content_width);  // Remove width to avoid duplicate padding
+	});
+	
 });
 
 
