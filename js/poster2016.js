959f2756f5043cd0c33cc10eb6d837660f815266
Jane <jane.e.reynolds@gmail.com>
2016-05-02 18:53:31 -0700

    Position tree markers

--- js/poster2016.js
+++ js/poster2016.js
@@ -1,4 +1,14 @@
 
+var poster_key = { 
+	'1': 'Bigleaf Maple',  
+	'2': 'Tulip Tree',
+	'3': 'Northern Catalpa',
+	'4': 'Black Willow',
+	'5': 'Southern Crabapple',
+	'6': 'Staghorn Sumac',
+	'7': 'Flowering Dogwood',
+	'8': 'Eastern Redbud'
+}
 
 /* * * ON READY * * */
 
@@ -12,25 +22,50 @@ $(document).ready(function(){
 	// Position markers
 	$("#1").css({left: '11%', top: '28%'});
 	$("#2").css({right: '8%', top: '25%'});
-	
+	$("#3").css({left: '11%', top: '48%'});
+	$("#4").css({right: '11%', top: '45%'});
+	$("#5").css({left: '13%', top: '65%'});
+	$("#6").css({right: '12%', top: '63%'});
+	$("#7").css({left: '11%', top: '78%'});
+	$("#8").css({right: '3%', top: '80%'});
 	
 	// Add pop-ups
 	var content_width = $("#info-window").width();
-	$("#1").hover(
+	
+	for (var marker in poster_key) {
+		if(poster_key.hasOwnProperty(marker)) {
+			console.log(marker+": "+poster_key[marker]);
+		}	
+	}
+/*	
+	$('.tree-mark').hover(
 		function(){
+			// Get content from poster_key hash and size info-window
+			$("#info-window").html(poster_key[this.id]);
+			content_width = $("#info-window").width(); 
+			alert("content_width: "+content_width);
+			$("#info-window").width(content_width + 20);  // Add extra width for padding
+			
+			// Position info-window
 			var marker_position = $(this).position();
-			//alert(marker_position.left + ", " + marker_position.top);
 			var left = marker_position.left + 50;
 			var top = marker_position.top;
-			$("#info-window").css({left: left, top: top}).html("Bigleaf Maple"); 
-			content_width = $("#info-window").width(); 
-			$("#info-window").width(content_width + 20);  // Add extra width for padding
+			if ( parseInt(this.id) % 2 === 0){
+				var right = $("#markers-container").width() - left + content_width;
+				$("#info-window").css({left: 'auto', right: right, top: top});
+			} else {
+				$("#info-window").css({left: left, right: 'auto', top: top});
+			}
+			
+			// Display
 			$("#info-window").toggle(); },
+			
 		function(){
 			$("#info-window").toggle();
 			$("#info-window").width(content_width);  // Remove width to avoid duplicate padding
 	});
 	
+*/	
 });
 
 
